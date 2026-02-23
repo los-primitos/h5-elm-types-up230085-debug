5ba55d3eab2f1c2f69286d83a154ae2a35d1228a
up230085-debug <up230085@alumnos.upa.edu.mx>
2026-02-22 20:01:38 -0600

    Tarea 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus grade =
+    if grade >= 7 then
+        Approved
+
+    else if grade < 0 then
+        Pending
+
+    else
+        Failed
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
